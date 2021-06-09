7731c933002e62160763295b6abf9c4bfccc020d
Steve <bryanstevedegea@gmail.com>
2021-06-09 13:08:30 -0500

    Reporte_Judaores

--- AdminitracionDeTorneosP/Program.cs
+++ AdminitracionDeTorneosP/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AdminitracionDeTorneosP.View;
 
 namespace AdminitracionDeTorneosP
 {
@@ -16,7 +17,7 @@ namespace AdminitracionDeTorneosP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Reporte_Jugadores1());
         }
     }
 }
